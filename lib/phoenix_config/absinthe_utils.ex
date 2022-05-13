984223f874405f4980babb9d377c3b709f10cd3b
MikaAK <mika@kalathil.me>
2022-05-13 00:11:56 -0700

    fix: swap naive_date time to proper type

--- lib/phoenix_config/absinthe_utils.ex
+++ lib/phoenix_config/absinthe_utils.ex
@@ -5,8 +5,8 @@ defmodule PhoenixConfig.AbsintheUtils do
 
   def normalize_ecto_type(:utc_datetime_usec), do: ":datetime"
   def normalize_ecto_type(:utc_datetime), do: ":datetime"
-  def normalize_ecto_type(:naive_datetime_usec), do: ":datetime"
-  def normalize_ecto_type(:naive_datetime), do: ":datetime"
+  def normalize_ecto_type(:naive_datetime_usec), do: ":naive_datetime"
+  def normalize_ecto_type(:naive_datetime), do: ":naive_datetime"
   def normalize_ecto_type(:time_usec), do: ":datetime"
   def normalize_ecto_type({:array, type}), do: "list_of(non_null(#{normalize_ecto_type(type)}))"
   def normalize_ecto_type(type), do: AbsintheGenerator.Type.maybe_string_atomize_type(inspect(type))
