3c8cd9aaa2546f1f85d2cf43194cd16cce525e61
Alan Vardy <alan@vardy.cc>
2023-06-14 09:21:38 -0700

    Fix doctor

---
+++ .doctor.exs
@@ -0,0 +1,15 @@
+%Doctor.Config{
+  exception_moduledoc_required: true,
+  failed: false,
+  ignore_modules: [],
+  ignore_paths: [],
+  min_module_doc_coverage: 0,
+  min_module_spec_coverage: 0,
+  min_overall_doc_coverage: 0,
+  min_overall_spec_coverage: 0,
+  min_overall_moduledoc_coverage: 0,
+  raise: false,
+  reporter: Doctor.Reporters.Full,
+  struct_type_spec_required: false,
+  umbrella: false
+}
